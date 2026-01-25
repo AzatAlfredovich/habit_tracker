8732f3a765d338996c6dd7a5a0019cb51200b3cc
Azat Khaliullin <AllisonUno@yandex.ru>
2026-01-25 22:50:12 +0400

    Добавлены настройки, пайплайн-файл, файлы Dockerfile и docker-compose

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 
@@ -80,6 +81,14 @@ REST_FRAMEWORK = {
     ],
 }
 
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "test_db_sqlite3",
+        }
+    }
+
 # Database
 DATABASES = {
     "default": {
@@ -158,3 +167,10 @@ CELERY_BEAT_SCHEDULE = {
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 TELEGRAM_URL = "https://api.telegram.org/bot"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+    }
+}
